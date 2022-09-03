f18dfa58db361439165040be812ba792ecce3e29
iamshiwei <2587159769@qq.com>
2022-09-03 11:15:32 +0800

    项目结构目录设置

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import jsonp from "jsonp";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
-  mounted() {
-    let url = "https://www.imooc.com/common/adver-getadver";
-    jsonp(url, (err, res) => {
-      console.log(res.data);
-    });
-  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
